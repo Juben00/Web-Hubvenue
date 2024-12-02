47d730407665236c7885da726dcfb72840c2e57b
ReizerJhn <reizerjhn@wmsu.edu.ph>
2024-12-03 04:01:55 +0800

    theme change

--- help-center.php
+++ help-center.php
@@ -52,7 +52,7 @@ $bookmarkIds = array_column($bookmarks, 'venue_id');
             right: 0.75rem;
             top: 50%;
             transform: translateY(-50%);
-            background-color: #ec4899;
+            background-color: #ef4444;
             color: white;
             padding: 0.5rem;
             border-radius: 9999px;
@@ -97,7 +97,7 @@ $bookmarkIds = array_column($bookmarks, 'venue_id');
                 Hi <?php echo isset($_SESSION['user']['firstname']) ? htmlspecialchars($_SESSION['user']['firstname']) : 'Guest'; ?>, how can we help?
             </h2>
             <div class="search-container">
-                <input type="text" placeholder="Search how-tos and more" class="focus:outline-none focus:ring-2 focus:ring-pink-500">
+                <input type="text" placeholder="Search how-tos and more" class="focus:outline-none focus:ring-2 focus:ring-red-500">
                 <button>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
